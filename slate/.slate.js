f91c81ad0703b44794d854fbd85500cfff7a2675
Aaron Trostle <aaron.trostle@gmail.com>
2016-05-22 10:22:06 -0700

    Update slate config, now reflects what im currently using, still need to work on layouts

--- slate/.slate.js
+++ slate/.slate.js
@@ -1,37 +1,71 @@
-// Create Operations
+// NOTE Monitors
+// var cinemaDisplay  = "2560x1440";
+// var retinaDisplay = "2880x1800";
+
+// NOTE defaults
+// slate.default(2, twoMonitorLayout)
+// slate.default(1, oneMonitorLayout)
+
+// NOTE operations
+// var lapFull = slate.operation("move", {
+//   "screen" : retinaDisplay,
+//   "x" : "screenOriginX",
+//   "y" : "screenOriginY",
+//   "width" : "screenSizeX",
+//   "height" : "screenSizeY"
+// });
+
+// NOTE common layout hashes
+// var lapFullHash = {
+//   "operations": [lapFull],
+//   "ignore-fail": true,
+//   "repeat": true
+// }
+
+// NOTE one monitor layout
+// var oneMonitorLayout = slate.layout("oneMonitor", {
+//   "Google Chrome": lapFullHash
+// });
+
+// var oneMonitor = S.op("layout", { "name" : oneMonitorLayout });
+// oneMonitor.run()
+
+// 1440, 896
+
+var corner = slate.operation("corner", {
+  "direction": "bottom-right",
+  "width": 478,
+  "height": 340,
+});
+
 var pushRight = slate.operation("push", {
-  "direction" : "right",
-  "style" : "bar-resize:(screenSizeX/3)*2"
-});
+  "direction": "right",
+  "style": "bar-resize:screenSizeX*0.75",
+})
+
 var pushLeft = slate.operation("push", {
-  "direction" : "left",
-  "style" : "bar-resize:(screenSizeX/3)*2"
+  "direction": "left",
+  "style": "bar-resize:screenSizeX*0.75",
 });
+
 var visor = slate.operation("move", {
   "x": "screenOriginX",
   "y": "screenOriginY",
   "width": "screenSizeX",
-  "height": "screenSizeY*0.75"
+  "height": "screenSizeY*0.75",
 })
+
 var fullscreen = slate.operation("move", {
   "x" : "screenOriginX",
   "y" : "screenOriginY",
   "width" : "screenSizeX",
-  "height" : "screenSizeY"
+  "height" : "screenSizeY",
 });
 
-S.bind("right:e,cmd", function(win) {
-    win.doOperation(pushRight);
-})
-
-S.bind("left:e,cmd", function(win) {
-    win.doOperation(pushLeft);
-})
-
-S.bind("up:e,cmd", function(win) {
-    win.doOperation(visor);
-})
-
-S.bind("down:e,cmd", function(win) {
-    win.doOperation(fullscreen);
+slate.bindAll({
+  "up:e,cmd": visor,
+  "down:e,cmd": fullscreen,
+  "right:e,cmd": pushRight,
+  "left:e,cmd": pushLeft,
+  "space:e,cmd": corner,
 })
